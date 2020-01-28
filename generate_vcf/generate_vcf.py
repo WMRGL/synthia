bddb71bfa59e585ed1b471ce364c3042ffead5b2
theocole <theo.j.cole@gmail.com>
2020-01-28 08:37:52 +0000

    moved location of input files

--- generate_vcf/generate_vcf.py
+++ generate_vcf/generate_vcf.py
@@ -12,29 +12,44 @@ browser:
     - table: RefSeq All
     - select: name, chrom, strand, exonStarts, exonEnds
 '''
+import argparse
 import numpy as np
 import pandas as pd
 
 
-df_canonical = pd.read_csv('UCSC_GRCh37_knownCanonical.tsv', sep='\t')
-df_exons = pd.read_csv('refSeq_GRCh37_all.tsv', sep='\t')
+class VCFGenerator():
+    def __init__(self):
+        pass
 
-df_canonical_idx = df_canonical.set_index('hg19.kgXref.refseq')
-df_exons_idx = df_exons.set_index('refSeqID')
+    def run(self):
+        df_canonical = pd.read_csv(self.canonical_transcripts, sep='\t')
+        df_canonical = df_canonical.set_index('hg19.kgXref.refseq')
 
-df_canonical_exons = df_canonical_exons.dropna()
-df_exons_for_bed = pd.concat(
-    [
-        pd.DataFrame(
-            data={
-                'chrom': row['#hg19.knownCanonical.chrom'], 
-                'chromStart': map(int, row.exonStarts.strip(',').split(',',)),
-                'chromEnd': map(int, row.exonEnds.strip(',').split(',')),
-                'name': map(
-                    lambda x: idx + '_exon' + str(x + 1), 
-                    range(len(row.exonStarts.strip(',').split(',')))
-                ), 
-                'strand': row.strand}) 
-        for idx, row in df_canonical_exons.iterrows()
-        ]
-).sort_values(by=["chrom", "chromStart"]).reset_index(drop=True)
+        df_exons = pd.read_csv(self.canonical_exons, sep='\t')
+        df_exons = df_exons.set_index('refSeqID')
+        df_exons['refSeqID'] = df_exons['#name'].str.split('.', expand=True)[0]
+
+
+    def create_canonical_exons_df():
+        df_canonical_exons = df_canonical.join(df_exons)
+        df_canonical_exons = df_canonical_exons.dropna()
+        df_exons_for_bed = pd.concat(
+            [
+                pd.DataFrame(
+                    data={
+                        'chrom': row['#hg19.knownCanonical.chrom'], 
+                        'chromStart': map(int, row.exonStarts.strip(',').split(',',)),
+                        'chromEnd': map(int, row.exonEnds.strip(',').split(',')),
+                        'name': map(
+                            lambda x: idx + '_exon' + str(x + 1), 
+                            range(len(row.exonStarts.strip(',').split(',')))
+                        ), 
+                        'strand': row.strand}) 
+                for idx, row in df_canonical_exons.iterrows()
+            ]
+        ).sort_values(by=["chrom", "chromStart"]).reset_index(drop=True)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Read in files for I/O.")
+    vcf_generator = VCFGenerator()
+    vcf_generator.run()
